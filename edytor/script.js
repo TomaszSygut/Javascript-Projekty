40ba350c18c9d480e7a199f8be87999b743c6b4b
Tomasz Sygut <thomassygut@gmail.com>
2019-03-14 09:30:00 -0700

    asadad

--- edytor/script.js
+++ edytor/script.js
@@ -1,124 +1,104 @@
-
 document.getElementById('photo').onchange = function(e) {
-    let img = new Image();
-    img.onload = draw;
-    img.onerror = failed;
-    img.src = URL.createObjectURL(this.files[0]); // Tworzy ścieżkę do pliku
-    
-  };
-  function draw() {
-    
-    let canvas = document.getElementById('myCanvas');
-    canvas.width = this.width;
-    canvas.height = this.height;
-    
-    let ctx = canvas.getContext('2d');
-    ctx.drawImage(this, 0,0);
-    
-   
-    
-  }
-  function failed() {
-    console.error("The provided file couldn't be loaded as an Image media");
-  }
+  let img = new Image();
+  img.onload = draw;
+  img.onerror = failed;
+  img.src = URL.createObjectURL(this.files[0]); // Tworzy ścieżkę do pliku
+  
+};
+function draw() {
+  
+  let canvas = document.getElementById('myCanvas');
+  canvas.width = this.width;
+  canvas.height = this.height;
+  
+  let ctx = canvas.getContext('2d');
+  ctx.drawImage(this, 0,0);
+  
+ 
+  
+}
+function failed() {
+  console.error("The provided file couldn't be loaded as an Image media");
+}
 
 
 window.onload = function()
 {
-    
-    const brightness = document.getElementById('brightness');
-    const contrast   = document.getElementById('contrast');
-    const sepia = document.getElementById('sepia');
-    const invert = document.getElementById('invert');
+  
+  const brightness = document.getElementById('brightness');
+  const contrast   = document.getElementById('contrast');
+  const sepia = document.getElementById('sepia');
+  const invert = document.getElementById('invert');
 
-    // filtry
-    contrast.onkeyup = contrast.onchange = function()
-     {
-         let contrast = document.getElementById('myCanvas'),
-           val = parseInt(this.value);
+  // filtry
+  contrast.onkeyup = contrast.onchange = function()
+   {
+       let contrast = document.getElementById('myCanvas'),
+         val = parseInt(this.value);
 
-             if (val > 100 || val < 0)
-         return false;
+           if (val > 100 || val < 0)
+       return false;
 
-        
-         contrast.style.filter = "contrast("+val+"%)";
-     }
+      
+       contrast.style.filter = "contrast("+val+"%)";
+   }
 
-    brightness.onkeyup = brightness.onchange = function()
-    {
-        let brightness = document.getElementById('myCanvas'),
-            val = parseInt(this.value);
+  brightness.onkeyup = brightness.onchange = function()
+  {
+      let brightness = document.getElementById('myCanvas'),
+          val = parseInt(this.value);
 
-            if (val > 100 || val < 0)
-        return false;
+          if (val > 100 || val < 0)
+      return false;
 
-        
-        brightness.style.filter = "brightness("+val+"%)";
-    }
+      
+      brightness.style.filter = "brightness("+val+"%)";
+  }
 
-    sepia.onkeyup = sepia.onchange = function()
-    {
-        let sepia = document.getElementById('myCanvas'),
-            val = parseInt(this.value);
+  sepia.onkeyup = sepia.onchange = function()
+  {
+      let sepia = document.getElementById('myCanvas'),
+          val = parseInt(this.value);
 
-            if (val > 100 || val < 0)
-        return false;
+          if (val > 100 || val < 0)
+      return false;
 
-        
-        sepia.style.filter = "sepia("+val+"%)";
-    }
-    invert.onkeyup = invert.onchange = function()
-    {
-        let invert = document.getElementById('myCanvas'),
-            val = parseInt(this.value);
+      
+      sepia.style.filter = "sepia("+val+"%)";
+  }
+  invert.onkeyup = invert.onchange = function()
+  {
+      let invert = document.getElementById('myCanvas'),
+          val = parseInt(this.value);
 
-            if (val > 100 || val < 0)
-        return false;
+          if (val > 100 || val < 0)
+      return false;
 
-        
-        invert.style.filter = "invert("+val+"%)";
-    }
+      
+      invert.style.filter = "invert("+val+"%)";
+  }
 
-    
-     
+  
+   
 }
-let points=new Array();	
-colorInput = document.getElementById("color");
-		function start(e){
-		  let mouseX = e.pageX - canvas.offsetLeft;
-		  let mouseY = e.pageY - canvas.offsetTop;
-		  paint = true;
-		  ctx.beginPath();
-		  ctx.moveTo(mouseX,mouseY);
-		  points[points.length]=[mouseX,mouseY];
-		};
 
-		function draww(e){
+      
+var el = document.getElementById('myCanvas');
+var ctx = el.getContext('2d');
+var isDrawing;
 
-		  if(paint){
-            let mouseX = e.pageX - canvas.offsetLeft;
-            let mouseY = e.pageY - canvas.offsetTop; 
-			 ctx.lineTo(mouseX,mouseY);
-			 ctx.stroke();	
-			 ctx.strokeStyle = colorInput.value;
-			 ctx.lineJoin = ctx.lineCap = 'round';
-
-			 points[points.length]=[mouseX,mouseY];
-		  }
-		  
-		}
-		function stop(e){
-		  paint=false;
-		//  var s=JSON.stringify(points);
-		// 	localStorage['lines']=s;
-		}  
-	
-
-		
-		let paint=false;
-		let canvas = document.getElementById('myCanvas');
-		let ctx=canvas.getContext("2d");
-		canvas.addEventListener('mousedown',start);
-		canvas.addEventListener('mousemove',draww);
-        canvas.addEventListener('mouseup',stop);
-        let color = document.getElementById('color');
+el.onmousedown = function(e) {
+  isDrawing = true;
+  ctx.lineWidth = 10;
+  ctx.lineJoin = ctx.lineCap = 'round';
+  ctx.moveTo(e.clientX, e.clientY);
+};
+el.onmousemove = function(e) {
+  if (isDrawing) {
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+  }
+};
+el.onmouseup = function() {
+  isDrawing = false;
+};
