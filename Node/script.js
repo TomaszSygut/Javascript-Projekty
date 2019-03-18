f52b7dc6d86262e83492e98668e575023518463d
Tomasz Sygut <thomassygut@gmail.com>
2019-03-18 21:58:54 +0100

    asasas

--- Node/script.js
+++ Node/script.js
@@ -23,7 +23,6 @@ class Storage{
 //localStorage
 let ls = new Storage();
 let notes = new Array();
-let pin = false;
 let retriveNotes = function(){
     notes = [];
     let data = ls.getNotes();
@@ -33,7 +32,7 @@ let retriveNotes = function(){
 //Zapisz przycisk
 let buttonSave = function(){
     //Tworzy nowy obiekt
-    n = new Note(document.querySelector('#title').value, document.querySelector('#note-color').value, pin, document.querySelector('#note').value);
+    n = new Note(document.querySelector('#title').value, document.querySelector('#note-color').value, document.querySelector('#pin').checked, document.querySelector('#note').value);
     notes.push(n);
     //Zapisuje do localstorage
     ls.saveNote(notes);
@@ -44,15 +43,26 @@ let buttonSave = function(){
 }
 
 let displayList = function(){
-    document.querySelector('#note-list-pin').innerHTML='';
+    document.querySelector('.note-list-pin').innerHTML='';
+    document.querySelector('.note-list-unpin').innerHTML='';
     notes.forEach(n => {
         //Tworzy HTML ELEMENT
         item = document.createElement('li');
+        title = document.createElement('p');
+        text = document.createElement('p');
         time = document.createElement('p');
         item.style.background = n.color;
-        item.innerHTML = n.title;
+        title.innerHTML = n.title;
+        text.innerHTML = n.text;
+        time.innerHTML = "Created: "+(new Date(n.id)).toLocaleString();
+        item.appendChild(title);
+        item.appendChild(text);
+        item.appendChild(time);
         item.onclick = function(){selectNote(n)}
-      document.querySelector('#note-list-pin').appendChild(item);
+        if(n.pinned)
+            document.querySelector('.note-list-pin').appendChild(item);
+        else
+            document.querySelector('.note-list-unpin').appendChild(item);
       
         
             
@@ -65,8 +75,6 @@ let selectNote = function(n) {
     document.querySelector('#note-color').value = n.color;
     document.querySelector('#pin').checked = n.pinned;
     document.querySelector('#note').value = n.text;
-    
-    pin = n.pinned;
 }
 //ładuje dane
 window.onload = function(){
